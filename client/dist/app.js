b5ac5d7cfbc48ee14f580f1d5ccf590f1b541860
emmaz05 <emmaffzheng@gmail.com>
2024-01-22 22:01:49 -0500

    map with icon

--- client/dist/app.js
+++ client/dist/app.js
@@ -1,3 +1,6 @@
+var map;
+var marker;
+
 function initMap() {
   var options = {
     center: { lat: 42.3601, lng: -71.0942 },
@@ -5,4 +8,31 @@ function initMap() {
   };
 
   map = new google.maps.Map(document.getElementById("map"), options);
+
+  const image = "ice cream.png";
+
+  marker = new google.maps.Marker({
+    position: { lat: 42.3601, lng: -71.0942 },
+    map,
+    title: "test post",
+    icon: image,
+  });
+
+  marker.addEventListener("click", function () {
+    openPostWindow();
+  });
+}
+
+function openPostWindow() {
+  var postContent =
+    '<div id="content">' +
+    '<h1 id="postTitle" class="postTitle">Sam Post</h1>' +
+    "<p>I love to eat.</p>" +
+    "</div>";
+
+  var postwindow = new google.maps.InfoWindow({
+    content: postContent,
+  });
+
+  postwindow.open(map, marker);
 }
